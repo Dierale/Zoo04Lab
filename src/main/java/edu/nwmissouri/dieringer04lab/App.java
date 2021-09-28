b37d90f94446e3da9b544c432d03e02722151b1c
Dierale <89657232+Dierale@users.noreply.github.com>
2021-09-27 21:02:21 -0500

    Initial commit

---
+++ src/main/java/edu/nwmissouri/dieringer04lab/App.java
@@ -0,0 +1,8 @@
+package edu.nwmissouri.dieringer04lab;
+
+public class App {
+    
+    public static void main(String[] args) {
+    }
+    
+}
---
+++ src/test/java/edu/nwmissouri/dieringer04lab/AppTest.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.dieringer04lab;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
